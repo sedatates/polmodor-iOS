97b53acac5beb4551f28ebc8412173d2643ce357
Sedat <sedat.ates0@icloud.com>
2025-02-14 13:45:42 +0300

    Initial Commit

---
+++ polmodorTests/polmodorTests.swift
@@ -0,0 +1,17 @@
+//
+//  polmodorTests.swift
+//  polmodorTests
+//
+//  Created by sedat ateş on 14.02.2025.
+//
+
+import Testing
+@testable import polmodor
+
+struct polmodorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
